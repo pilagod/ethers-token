7ce57e8b1a30c0e48d21afbaf667b02a1fc4c17b
pilagod <pilagooood@gmail.com>
2022-06-25 14:21:10 +0800

    setup hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,10 @@
+import "dotenv/config"
+import "tsconfig-paths/register"
+import "@nomiclabs/hardhat-ethers"
+import "@nomiclabs/hardhat-waffle"
+
+module.exports = {
+    networks: {
+        hardhat: {},
+    },
+}
